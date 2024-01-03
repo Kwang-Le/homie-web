51dd04d14c12e3ed9170ffb4b0925a1d9fed8482
Kwang_Le <quangle280302@gmail.com>
2024-01-03 09:54:27 +0700

    fix path issues leading to deployment errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-import DashboardView from '../views/DashboardView.vue'
+import DashboardView from '../views/dashboard/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
