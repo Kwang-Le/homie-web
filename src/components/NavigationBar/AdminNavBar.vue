038431edd45ab56e1b35e0aa860ef9a133eaba34
namdeptrai1102 <109681639+namdeptrai1102@users.noreply.github.com>
2024-01-03 23:29:35 +0700

    refactor: add empty screen

--- src/components/NavigationBar/AdminNavBar.vue
+++ src/components/NavigationBar/AdminNavBar.vue
@@ -7,31 +7,58 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="px-5 collapse navbar-collapse justify-content-between" id="navbarNav">
-              <router-link class="nav-link active" aria-current="page" to="/admin-dashboard">Trang chủ</router-link>
-              <a class="nav-link" href="#">Tài khoản</a>
-                        <a class="nav-link" href="#">Căn hộ</a>
-                        <a class="nav-link" href="#">Cư dân</a>
-                        <a class="nav-link" href="#">Khoản thu</a>
-                        <a class="nav-link" href="#">Thống kê</a>
-              <router-link class="nav-link" to="/admin-notification">Thông báo</router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'dashboard' }" to="/admin-dashboard" @click="setActiveLink('general')">Trang chủ </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'account' }" to="/admin-account" @click="setActiveLink('general')">Tài khoản </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'apartment' }" to="/admin-apartment" @click="setActiveLink('general')">Căn hộ </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'resident' }" to="/admin-resident" @click="setActiveLink('general')">Cư dân </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'revenue' }" to="/admin-revenue" @click="setActiveLink('general')">Khoản thu </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'statistical' }" to="/admin-statistical-" @click="setActiveLink('general')">Thống kê </router-link>
+              <router-link class="nav-link" :class="{ active: activeLink === 'notification' }" to="/admin-notification" @click="setActiveLink('general')">Thông báo</router-link>
               <router-link class="nav-link" to="/admin-setting">Cài đặt</router-link>
+
+
             </div>
         </div>
     </nav>
 </template>
-
+<script>
+export default {
+  name: 'AdminNavBar',
+  data() {
+    return {
+      activeLink: 'dashboard' // Mặc định nav-link "Trang chủ" sẽ được chọn
+    }
+  },
+  methods: {
+    setActiveLink(link) {
+      this.activeLink = link; // Thay đổi nav-link được chọn khi người dùng click vào một nav-link khác
+    }
+  }
+}
+</script>
 <style scoped>
 .navbar {
     background: #F1FAEE;
     border-radius: 15px;
 }
 .nav-link {
+  display: flex;
   padding: 10px;
   color: #1D3557;
   text-decoration: none;
   font-family: 'Raleway', sans-serif;
   font-weight: bold;
   font-size: 20px;
+  border-radius: 15px;
+}
+.nav-link.active {
+  background-color: #457B9D;
+  color: #F1FAEE;
+}
+.nav-link:active, .nav-link:focus {
+  background-color: #457B9D;
+  color: #F1FAEE;
+  outline: none;
 }
 </style>
 <script setup>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import DashboardView from '@/views/admin/AdminDashboardView.vue'
 import AdminNotificationView from '@/views/admin/AdminNotificationView.vue'
 import AdminDashboardView from '@/views/admin/AdminDashboardView.vue'
 import AdminSettingView from '@/views/admin/AdminSettingView.vue'
+import AdminAccountView from '@/views/admin/AdminAccountView.vue'
+import AdminApartmentView from '@/views/admin/AdminApartmentView.vue'
+import AdminResidentView from '@/views/admin/AdminResidentView.vue'
+import AdminRevenueView from '@/views/admin/AdminRevenueView.vue'
+import AdminStatisticalView from '@/views/admin/AdminStatisticalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +16,31 @@ const router = createRouter({
       path: '/admin-dashboard',
       name: 'admin-dashboard',
       component: AdminDashboardView
+    },
+    {
+      path: '/admin-account',
+      name: 'admin-account',
+      component: AdminAccountView
+    },
+    {
+      path: '/admin-apartment',
+      name: 'admin-apartment',
+      component: AdminApartmentView
+    },
+    {
+      path: '/admin-resident',
+      name: 'admin-resident',
+      component: AdminResidentView
+    },
+    {
+      path: '/admin-revenue',
+      name: 'admin-revenue',
+      component: AdminRevenueView
+    },
+    {
+      path: '/admin-statistical',
+      name: 'admin-statistical',
+      component: AdminStatisticalView
     },
     {
       path: '/admin-notification',
