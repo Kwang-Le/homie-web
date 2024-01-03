6797ab6c8673d6a4a6e15d4e16171035bdbfdd85
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-04 01:22:37 +0700

    feat(router): add routes for personal info view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardView from '../views/dashboard/DashboardView.vue'
+import PersonalInfoViewVue from '@/views/personal-info/PersonalInfoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'dashboard',
       component: DashboardView
+    },
+    {
+      path: '/personal-info',
+      name: 'personal-info',
+      component: PersonalInfoViewVue
     },
   ]
 })
