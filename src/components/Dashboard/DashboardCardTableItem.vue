c7718996d5cb1e5e0f52d83e6d3dec528ce9c2b7
Kwang_Le <quangle280302@gmail.com>
2024-01-04 14:16:34 +0700

    add css and modifies to dashboardTableComponent

--- src/components/Dashboard/DashboardCardTableItem.vue
+++ src/components/Dashboard/DashboardCardTableItem.vue
@@ -3,12 +3,18 @@
 <template>
     <h5 class="card-title font-weight-bold">Các khoản thu mới cập nhật</h5>
     <div class="d-flex flex-column card-text">
-        <div class="table-row d-flex justify-content-between px-4">
-            <p>Hello</p>
-            <div class="fee-box">1,000,000</div>
+        <div class="table-row d-flex justify-content-between px-4 py-1" v-for="data in tableData">
+            <div class="row-title p-1">Tiền điện hộ 304</div>
+            <div class="row-amount p-1">1,000,000</div>
         </div>
-        <div class="table-row d-flex">Hello</div>
-        <div class="table-row d-flex">Hello</div>
+        <div class="table-row d-flex justify-content-between px-4 py-1">
+            <div class="row-title p-1">Tiền nước hộ 101</div>
+            <div class="row-amount p-1">1,000,000</div>
+        </div>
+        <div class="table-row d-flex justify-content-between px-4 py-1">
+            <div class="row-title p-1">Tiền vệ sinh hộ 205</div>
+            <div class="row-amount p-1">1,000,000</div>
+        </div>
     </div>
 </template>
 
@@ -16,9 +22,7 @@
 export default {
     name: "DashboardCardTableItem",
     props: {
-        title: String,
-        content: String,
-        chart: Object
+        tableData: Object
     },
 };
 </script>
@@ -26,11 +30,19 @@ export default {
 <style scoped>
 .table-row {
     background-color: var(--btn-alternative);
-    height: 20px;
+    /* height: 20px; */
     width: auto;
     border: 1px solid black;
 }
-.fee-box {
-    
+.row-title {
+    text-align: center;
+    color: black;
+}
+.row-amount {
+    color: white;
+    background-color: var(--main-primary);
+    font-size: smaller;
+    border: 1px solid black;
+    border-radius: 8px;
 }
 </style>
