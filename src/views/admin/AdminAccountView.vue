274dc952d06a455553b05c744b1de9a3f94a2210
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-04 18:37:40 +0700

    fix: reassign views

--- src/views/admin/AdminAccountView.vue
+++ src/views/admin/AdminAccountView.vue
@@ -1,26 +1,12 @@
 <script setup>
-// import Card from '@/components/Card.vue';
-import ExtraProfileCardItem from '@/components/Profile/ExtraProfileCardItem.vue';
-import MainProfile from '@/components/Profile/MainProfile.vue';
+
 </script>
 
 <template>
   <main>
-    <div class="row">
-      <div class="col-8">
-        <MainProfile></MainProfile>
-      </div>
-      <div class="col-4">
-          <h4 class="card-title font-weight-bold">Người sống cùng</h4>
-          <ExtraProfileCardItem />
-      </div>
-    </div>
+
+
   </main>
 </template>
 
-<style scoped>
-  .card-title {
-    text-align: center;
-
-  }
-</style>
+<style></style>
