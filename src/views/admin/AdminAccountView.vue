2aec17419d5ed67eb3e7bd501549f56a0e073a84
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-04 14:38:47 +0700

    refactor: move personal info to AdminAccountView

--- src/views/admin/AdminAccountView.vue
+++ src/views/admin/AdminAccountView.vue
@@ -1,12 +1,35 @@
 <script setup>
-
+import Card from '@/components/Card.vue';
 </script>
 
 <template>
-  <main>
+    <main>
+        <div class="row">
+            <div class="col-8">
+                <Card>
+                    <div class="row">
+                        <div class="col-3">
+                            <!-- profile pic and name -->
+                        </div>
 
+                        <div class="col-4">
+                            <!-- personal info -->
+                        </div>
 
-  </main>
+                        <div class="col-4">
+                            <!-- apartment-related info -->
+                        </div>
+                    </div>
+                </Card>
+            </div>
+            <div class="col-4">
+                <Card>
+                    <h5 class="card-title font-weight-bold">Người sống cùng</h5>
+                    <Card>
+                        <!-- DetailsCard -->
+                    </Card>
+                </Card>
+            </div>
+        </div>
+    </main>
 </template>
-
-<style></style>
