fd49cd3c64bd8368d52774ad7b360894836dbc68
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-04 15:31:37 +0700

    feat: update AdminAccountView

--- src/views/admin/AdminAccountView.vue
+++ src/views/admin/AdminAccountView.vue
@@ -1,35 +1,25 @@
 <script setup>
-import Card from '@/components/Card.vue';
+// import Card from '@/components/Card.vue';
+import ExtraProfileCardItem from '@/components/ExtraProfile/ExtraProfileCardItem.vue';
+import MainProfile from '@/components/Profile/MainProfile.vue';
 </script>
 
 <template>
-    <main>
-        <div class="row">
-            <div class="col-8">
-                <Card>
-                    <div class="row">
-                        <div class="col-3">
-                            <!-- profile pic and name -->
-                        </div>
-
-                        <div class="col-4">
-                            <!-- personal info -->
-                        </div>
-
-                        <div class="col-4">
-                            <!-- apartment-related info -->
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div class="col-4">
-                <Card>
-                    <h5 class="card-title font-weight-bold">Người sống cùng</h5>
-                    <Card>
-                        <!-- DetailsCard -->
-                    </Card>
-                </Card>
-            </div>
-        </div>
-    </main>
+  <main>
+    <div class="row">
+      <div class="col-8">
+        <MainProfile></MainProfile>
+      </div>
+      <div class="col-4">
+          <h5 class="card-title font-weight-bold">Người sống cùng</h5>
+          <ExtraProfileCardItem />
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+  .h5 {
+    text-align: center;
+  }
+</style>
