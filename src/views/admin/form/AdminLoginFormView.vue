ebf6f0d080808749de455e1bb147365b0da44f8a
shirakamipc <duongnhatthanh2001@gmail.com>
2024-01-06 15:08:37 +0700

    build form template

---
+++ src/views/admin/form/AdminLoginFormView.vue
@@ -0,0 +1,430 @@
+<template>
+  <div class="ng-nh-p-role-qu-n-tr">
+    <svg
+      class="screen"
+      width="1926"
+      height="1086"
+      viewBox="0 0 1926 1086"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask
+        id="mask0_939_16793"
+        style="mask-type: luminance"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="1926"
+        height="1086"
+      >
+        <path
+          d="M1903.8 3H22.2C11.5961 3 3 11.0589 3 21V1065C3 1074.94 11.5961 1083 22.2 1083H1903.8C1914.4 1083 1923 1074.94 1923 1065V21C1923 11.0589 1914.4 3 1903.8 3Z"
+          fill="white"
+          stroke="#CAD6E2"
+          stroke-width="6"
+        />
+      </mask>
+      <g mask="url(#mask0_939_16793)">
+        <path
+          d="M1903.8 3H22.2C11.5961 3 3 11.0589 3 21V1065C3 1074.94 11.5961 1083 22.2 1083H1903.8C1914.4 1083 1923 1074.94 1923 1065V21C1923 11.0589 1914.4 3 1903.8 3Z"
+          fill="white"
+          stroke="#CAD6E2"
+          stroke-width="6"
+        />
+      </g>
+    </svg>
+
+    <div class="group-2281">
+      <div class="shape-with-text"></div>
+      <div class="ng-nh-p">Đăng nhập</div>
+      <div class="email">Email</div>
+      <div class="ng-nh-p-v-i-vai-tr">Đăng nhập với vai trò</div>
+      <div class="password">Password</div>
+      <div class="group-n-btn">
+        <div class="rectangle-23"></div>
+        <div class="ng-nh-p2">Đăng nhập</div>
+      </div>
+      <div class="rectangle-24"></div>
+      <div class="rectangle-25"></div>
+      <div class="useremail-123-gmail-com">useremail123@gmail.com</div>
+      <div class="group-2282">
+        <div class="ellipse-16"></div>
+        <div class="ellipse-17"></div>
+        <div class="ellipse-18"></div>
+        <div class="ellipse-19"></div>
+        <div class="ellipse-21"></div>
+        <div class="ellipse-22"></div>
+        <div class="ellipse-20"></div>
+        <div class="ellipse-23"></div>
+      </div>
+      <div class="app-ava">
+        <div class="ellipse-24"></div>
+        <img class="image-1" src="https://s3-alpha-sig.figma.com/img/cb71/8914/11bc46088a6b7a902d97a5eda65a4a14?Expires=1705276800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DbpOSj-G7R6Y-nx6xq-YQaosUnKGGKzgUu8lQqDfgQOQE6w4C9hBlF3vr11KMYJAPHrOj8~czeLv1qef2egnEqJ4xS65kD~4uo6NdwIYab07kniKCH~UUsdZjM7-ZVh3JIZdKn2kNOOFDCdd72WedNLVyyTMEfzKDcTFRbHoK4UTMR6~IVLhinrq2iJx6Z~fsE84IT4zSsWNngCLUhjkn74P~mNO0w~mRXMRDr1~BoLO10Em~kvP0eCKl87udZP0tHngT-GbSDUcAjVQmMQ05nEzjmUQRuwa9oIgq~ALZ4kQIMASH5RLSXXTobAZpnkFvfT6qBx~cr1nE-RctuvQhA__" />
+      </div>
+      <div class="dropdown">
+        <div class="frame">
+          <div class="frame2"></div>
+          <div class="frame3">
+            <div class="role">Quản trị</div>
+            <svg
+              class="arrow-drop-down"
+              width="28"
+              height="27"
+              viewBox="0 0 28 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.24121 10.856L13.7889 16.4036L19.3366 10.856H8.24121Z"
+                fill="#666666"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="qu-n-m-t-kh-u">Quên mật khẩu</div>
+    </div>
+  </div>
+</template>
+<script>
+/* Code generated with AutoHTML Plugin for Figma */
+
+export default {
+  name: "NgNhPRoleQuNTr",
+  components: {},
+  props: {},
+  data() {
+    // quickfix to have components available to pass as props
+    return {};
+  },
+};
+</script>
+<style scoped>
+.ng-nh-p-role-qu-n-tr,
+.ng-nh-p-role-qu-n-tr * {
+  box-sizing: border-box;
+}
+.ng-nh-p-role-qu-n-tr {
+  width: 1920px;
+  height: 1080px;
+  position: relative;
+}
+.screen {
+  height: auto;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: visible;
+}
+.group-2281 {
+  position: absolute;
+  inset: 0;
+}
+.shape-with-text {
+  background: #a8dadc;
+  border-radius: 0px;
+  width: 1060px;
+  height: 685px;
+  position: absolute;
+  left: 430px;
+  top: 248px;
+}
+.ng-nh-p {
+  color: #1d3557;
+  text-align: left;
+  font-family: "Raleway-Bold", sans-serif;
+  font-size: 36px;
+  line-height: 150%;
+  font-weight: 700;
+  position: absolute;
+  left: 885.23px;
+  top: 295.16px;
+  width: 260.27px;
+  height: 54.42px;
+}
+.email {
+  color: #1d3557;
+  text-align: left;
+  font-family: "Raleway-Medium", sans-serif;
+  font-size: 24px;
+  line-height: 150%;
+  font-weight: 500;
+  position: absolute;
+  left: 885.23px;
+  top: 467px;
+  width: 70.98px;
+  height: 32.65px;
+}
+.ng-nh-p-v-i-vai-tr {
+  color: #1d3557;
+  text-align: left;
+  font-family: "Raleway-Medium", sans-serif;
+  font-size: 24px;
+  line-height: 150%;
+  font-weight: 500;
+  position: absolute;
+  left: 885.23px;
+  top: 391px;
+  width: 250px;
+  height: 32.65px;
+}
+.password {
+  color: #1d3557;
+  text-align: left;
+  font-family: "Raleway-Medium", sans-serif;
+  font-size: 24px;
+  line-height: 150%;
+  font-weight: 500;
+  position: absolute;
+  left: 885.23px;
+  top: 595.05px;
+  width: 104.11px;
+  height: 32.91px;
+}
+.group-n-btn {
+  width: 515.8px;
+  height: 90px;
+  position: static;
+}
+.rectangle-23 {
+  background: #457b9d;
+  border-radius: 10px;
+  width: 515.8px;
+  height: 68.93px;
+  position: absolute;
+  left: 885px;
+  top: 733px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.ng-nh-p2 {
+  color: #ffffff;
+  text-align: center;
+  font-family: "Raleway-SemiBold", sans-serif;
+  font-size: 28px;
+  line-height: 150%;
+  font-weight: 600;
+  position: absolute;
+  left: calc(50% - -75px);
+  top: calc(50% - -206px);
+  width: 216px;
+  height: 77px;
+}
+.rectangle-24 {
+  background: #ffffff;
+  border-radius: 10px;
+  width: 515.8px;
+  height: 68.93px;
+  position: absolute;
+  left: 885.23px;
+  top: 512.35px;
+  box-shadow: inset 0px 4px 4px 0px rgba(0, 0, 0, 0.25),
+    0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.rectangle-25 {
+  background: #ffffff;
+  border-radius: 10px;
+  width: 515.8px;
+  height: 68.93px;
+  position: absolute;
+  left: 885.23px;
+  top: 640.23px;
+  box-shadow: inset 0px 4px 4px 0px rgba(0, 0, 0, 0.25),
+    0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.useremail-123-gmail-com {
+  color: #000000;
+  text-align: left;
+  font-family: "Roboto-Regular", sans-serif;
+  font-size: 24px;
+  line-height: 150%;
+  font-weight: 400;
+  position: absolute;
+  left: 901.32px;
+  top: 529.58px;
+  width: 388.04px;
+  height: 32.65px;
+}
+.group-2282 {
+  width: 167.52px;
+  height: 16.33px;
+  position: static;
+}
+.ellipse-16 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 913.62px;
+  top: 666.53px;
+}
+.ellipse-17 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 935.39px;
+  top: 666.53px;
+}
+.ellipse-18 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 956.21px;
+  top: 666.53px;
+}
+.ellipse-19 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 977.98px;
+  top: 666.53px;
+}
+.ellipse-21 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 1021.52px;
+  top: 666.53px;
+}
+.ellipse-22 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 1042.34px;
+  top: 666.53px;
+}
+.ellipse-20 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 999.75px;
+  top: 666.53px;
+}
+.ellipse-23 {
+  background: #1e1e1e;
+  border-radius: 50%;
+  width: 17.04px;
+  height: 16.33px;
+  position: absolute;
+  left: 1064.11px;
+  top: 666.53px;
+}
+.app-ava {
+  width: 300px;
+  height: 300px;
+  position: static;
+}
+.ellipse-24 {
+  background: #f6f6f6;
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  left: 527.48px;
+  top: 440px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  filter: blur(2px);
+}
+.image-1 {
+  border-radius: 1000px;
+  width: 280.31px;
+  height: 293.95px;
+  position: absolute;
+  left: 537.8px;
+  top: 444.65px;
+}
+.dropdown {
+  display: flex;
+  flex-direction: column;
+  gap: 4.44px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  position: absolute;
+  right: 28.21%;
+  left: 58.77%;
+  width: 13.02%;
+  bottom: 59.81%;
+  top: 35.28%;
+  height: 4.91%;
+}
+.frame {
+  background: var(--white, #ffffff);
+  border-radius: 4.44px;
+  border-style: solid;
+  border-color: var(--gray-200, #b3b3b3);
+  border-width: 1.11px;
+  display: flex;
+  flex-direction: column;
+  gap: 0px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  align-self: stretch;
+  flex-shrink: 0;
+  position: relative;
+}
+.frame2 {
+  background: var(--white, #ffffff);
+  align-self: stretch;
+  flex-shrink: 0;
+  height: 0px;
+  position: relative;
+}
+.frame3 {
+  padding: 13.31px 13.31px 13.31px 17.75px;
+  display: flex;
+  flex-direction: row;
+  gap: 13.31px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  align-self: stretch;
+  flex-shrink: 0;
+  position: relative;
+}
+.role {
+  color: var(--gray-100, #666666);
+  text-align: left;
+  font-family: var(
+    --android-subtitle-1-font-family,
+    "BalsamiqSans-Regular",
+    sans-serif
+  );
+  font-size: var(--android-subtitle-1-font-size, 16px);
+  line-height: var(--android-subtitle-1-line-height, 24px);
+  font-weight: var(--android-subtitle-1-font-weight, 400);
+  position: relative;
+  flex: 1;
+}
+.arrow-drop-down {
+  flex-shrink: 0;
+  width: 26.63px;
+  height: 26.63px;
+  position: relative;
+  overflow: visible;
+}
+.qu-n-m-t-kh-u {
+  color: #1d3557;
+  text-align: left;
+  font-family: "Raleway-Medium", sans-serif;
+  font-size: 24px;
+  line-height: 150%;
+  font-weight: 500;
+  text-decoration: underline;
+  position: absolute;
+  left: 885px;
+  top: 816px;
+  width: 196px;
+  height: 32px;
+}
+</style>
---
+++ src/views/admin/form/AdminNewFeePopUpFormView.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="th-ng-b-o-t-i-template">
+    <div class="rectangle-30"></div>
+    <div class="m-u-danh-s-ch-c-t-i-xu-ng-h-y-i-n-th-ng-tin-theo-m-u-danh-s-ch">
+      Mẫu danh sách đã được tải xuống, hãy điền thông tin theo mẫu danh sách.
+    </div>
+    <svg
+      class="icon-x-letter"
+      width="28"
+      height="29"
+      viewBox="0 0 28 29"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_1032_5105)">
+        <path
+          d="M24 1.36762L22.6792 0.046875L14 8.72608L5.32085 0.046875L4 1.36762L12.6792 10.0469L4 18.7261L5.32085 20.0469L14 11.3677L22.6792 20.0469L24 18.7261L15.3208 10.0469L24 1.36762Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_1032_5105"
+          x="0"
+          y="0.046875"
+          width="28"
+          height="28"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1032_5105"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1032_5105"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  </div>
+</template>
+<script>
+/* Code generated with AutoHTML Plugin for Figma */
+
+export default {
+  name: "ThNgBOTITemplate",
+  components: {},
+  props: {},
+  data() {
+    // quickfix to have components available to pass as props
+    return {};
+  },
+};
+</script>
+<style scoped>
+.th-ng-b-o-t-i-template,
+.th-ng-b-o-t-i-template * {
+  box-sizing: border-box;
+}
+.th-ng-b-o-t-i-template {
+  padding: 0px 2px 0px 2px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  position: relative;
+}
+.rectangle-30 {
+  background: #a8dadc;
+  border-radius: 10px;
+  border-style: solid;
+  border-color: #000000;
+  border-width: 2px;
+  flex-shrink: 0;
+  width: 895px;
+  height: 346px;
+  position: relative;
+  box-shadow: 4px 4px 3px 1px rgba(0, 0, 0, 0.25);
+}
+.m-u-danh-s-ch-c-t-i-xu-ng-h-y-i-n-th-ng-tin-theo-m-u-danh-s-ch {
+  color: #1d3557;
+  text-align: center;
+  font-family: "Raleway-Bold", sans-serif;
+  font-size: 32px;
+  line-height: 150%;
+  font-weight: 700;
+  position: absolute;
+  left: calc(50% - 314.5px);
+  top: calc(50% - 53px);
+  width: 630px;
+  height: 106px;
+}
+.icon-x-letter {
+  flex-shrink: 0;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 1.11%;
+  left: 96.66%;
+  width: 2.22%;
+  bottom: 91.89%;
+  top: 2.33%;
+  height: 5.78%;
+  overflow: visible;
+}
+</style>
