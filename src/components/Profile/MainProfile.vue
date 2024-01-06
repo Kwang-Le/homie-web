0fe464b1f610f86686e5d31fd5d06cba967e8262
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-06 17:23:03 +0700

    feat: add labels to MainProfile view

--- src/components/Profile/MainProfile.vue
+++ src/components/Profile/MainProfile.vue
@@ -17,8 +17,66 @@ import Card from '@/components/Card.vue';
                     </div>
                     <div class="main-avt-name">Lê Thanh Quang</div>
                 </div>
-                <div class="col-4"></div>
-                <div class="col-4"></div>
+                <div class="col-4">
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Giới tính</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>Nam</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Ngày sinh</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>28/03/2002</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">CCCD</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>038202000015</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Email</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>quangle@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Số điện thoại</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>0852589862</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Căn hộ</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>P2011</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="info-label">
+                            <h5 class="info-title font-weight-bold">Trạng thái</h5>
+                            <div class="info-content d-flex flex-column card-text">
+                                <p>Tạm trú</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Card>
     </main>
@@ -27,6 +85,7 @@ import Card from '@/components/Card.vue';
 <style scoped>
 .row {
     margin-top: 10px;
+    /* needs review */
 }
 
 .col-4.avatar {
@@ -60,5 +119,23 @@ import Card from '@/components/Card.vue';
     white-space: nowrap;
     font: 30px/150% Raleway, sans-serif;
     text-align: center;
+}
+
+.info-label {
+    /* font: 18px/150% Raleway, sans-serif; */
+}
+
+.info-title {
+    font: bold 24px/150% Raleway, sans-serif;
+    font-size: 24px;
+    line-height: 150%;
+    /* font-weight: bold; */
+}
+
+.info-content {
+    font: normal 18px/150% Raleway, sans-serif;
+    /* font-size: 18px; */
+    font-weight: normal;
+    color: var(--main-secondary);
 }
 </style>
