e177b8479af207190cbc54afb8971aa8fe0035c8
shirakamipc <duongnhatthanh2001@gmail.com>
2024-01-07 21:52:02 +0700

    form not complete

--- src/views/admin/form/AdminNewApartmentFormView.vue
+++ src/views/admin/form/AdminNewApartmentFormView.vue
@@ -4,36 +4,26 @@
     <div class="frame">
       <div class="frame2"></div>
       <div class="frame3">
-        <div class="t-nguy-n">Mua</div>
-        <svg
-          class="arrow-drop-down"
-          width="27"
-          height="27"
-          viewBox="0 0 27 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.82349 10.8561L13.3712 16.4038L18.9188 10.8561H7.82349Z"
-            fill="#666666"
-          />
-        </svg>
+        <select class="t-nguy-n">
+          <option>Mua</option>
+          <option>Thuê</option>
+        </select>
       </div>
     </div>
+
     <div class="ghi-nh-n-c-n-h-m-i">Ghi nhận căn hộ mới</div>
     <div class="group-n-btn">
       <div>
-      <button class="rectangle-23" @click="handleClick">Tạo mới</button>
-    </div>
+        <button class="rectangle-23" @click="handleClick">Tạo mới</button>
+      </div>
     </div>
     <div class="t-ng">Tầng</div>
     <div class="t-i-a-ch">
       <div class="text-field">
         <div class="text-field2">
-          <div class="frame4">
-            <div class="frame2"></div>
+          <div class="frame2">
             <div>
-              <input class="frame5" placeholder="1234">
+              <input class="frame5" type="text" placeholder="1234">
             </div>
           </div>
         </div>
@@ -45,53 +35,53 @@
     <div class="ch-h">Chủ hộ</div>
     <div class="lo-i-h-nh">Loại hình</div>
     <svg
-      class="icon-x-letter"
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+        class="icon-x-letter"
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
     >
       <g filter="url(#filter0_d_403_12028)">
         <path
-          d="M36 2.11317L33.8866 0L20 13.8866L6.11336 0L4 2.11317L17.8867 15.9999L4 29.8866L6.11336 31.9998L20 18.1132L33.8866 31.9998L36 29.8866L22.1133 15.9999L36 2.11317Z"
-          fill="black"
+            d="M36 2.11317L33.8866 0L20 13.8866L6.11336 0L4 2.11317L17.8867 15.9999L4 29.8866L6.11336 31.9998L20 18.1132L33.8866 31.9998L36 29.8866L22.1133 15.9999L36 2.11317Z"
+            fill="black"
         />
       </g>
       <defs>
         <filter
-          id="filter0_d_403_12028"
-          x="0"
-          y="0"
-          width="40"
-          height="39.9998"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+            id="filter0_d_403_12028"
+            x="0"
+            y="0"
+            width="40"
+            height="39.9998"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
           <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
           />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
           <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
           />
           <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_403_12028"
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_403_12028"
           />
           <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_403_12028"
-            result="shape"
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_403_12028"
+              result="shape"
           />
         </filter>
       </defs>
@@ -101,20 +91,10 @@
       <div class="frame6">
         <div class="frame2"></div>
         <div class="frame3">
-          <div class="t-nguy-n">Sừ dụng</div>
-          <svg
-            class="arrow-drop-down2"
-            width="27"
-            height="27"
-            viewBox="0 0 27 27"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.82324 10.8561L13.3709 16.4038L18.9186 10.8561H7.82324Z"
-              fill="#666666"
-            />
-          </svg>
+          <select class="t-nguy-n">
+            <option>16</option>
+            <option>17</option>
+          </select>
         </div>
       </div>
     </div>
@@ -122,43 +102,22 @@
       <div class="frame6">
         <div class="frame2"></div>
         <div class="frame3">
-          <div class="t-nguy-n">2</div>
-          <svg
-            class="arrow-drop-down3"
-            width="27"
-            height="27"
-            viewBox="0 0 27 27"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.82324 10.8561L13.3709 16.4038L18.9186 10.8561H7.82324Z"
-              fill="#666666"
-            />
-          </svg>
+          <select class="t-nguy-n">
+            <option>1601</option>
+            <option>1602</option>
+          </select>
         </div>
       </div>
     </div>
+    <div class="th-m-nh-n-kh-u">Thêm nhân khẩu</div>
     <div class="frame7">
-      <div class="frame2"></div>
       <div class="frame3">
-        <div class="t-nguy-n">Lê Thanh Quang</div>
-        <svg
-          class="arrow-drop-down4"
-          width="27"
-          height="27"
-          viewBox="0 0 27 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.82349 10.8561L13.3712 16.4038L18.9188 10.8561H7.82349Z"
-            fill="#666666"
-          />
-        </svg>
+        <select class="t-nguy-n">
+          <option>Dương Nhật Thành</option>
+          <option>Lê Thanh Quang</option>
+        </select>
       </div>
     </div>
-    <div class="th-m-nh-n-kh-u">Thêm nhân khẩu</div>
   </div>
 </template>
 <script>
@@ -186,11 +145,13 @@ export default {
 .c-n-h-form-ghi-nh-n-s-d-ng-c-n-h-m-i * {
   box-sizing: border-box;
 }
+
 .c-n-h-form-ghi-nh-n-s-d-ng-c-n-h-m-i {
   width: 1120px;
   height: 645px;
   position: relative;
 }
+
 .rectangle-30 {
   background: #a8dadc;
   border-radius: 10px;
@@ -204,6 +165,7 @@ export default {
   top: 0px;
   box-shadow: 4px 4px 3px 1px rgba(0, 0, 0, 0.25);
 }
+
 .frame {
   background: var(--white, #ffffff);
   border-radius: 4.44px;
@@ -220,6 +182,7 @@ export default {
   left: 746px;
   top: 172px;
 }
+
 .frame2 {
   background: var(--white, #ffffff);
   align-self: stretch;
@@ -227,8 +190,8 @@ export default {
   height: 0px;
   position: relative;
 }
+
 .frame3 {
-  padding: 13.31px 13.31px 13.31px 17.75px;
   display: flex;
   flex-direction: row;
   gap: 13.31px;
@@ -238,7 +201,9 @@ export default {
   flex-shrink: 0;
   position: relative;
 }
+
 .t-nguy-n {
+  padding: 13.31px 13.31px 13.31px 17.75px;
   color: var(--gray-100, #666666);
   text-align: left;
   font-family: "BalsamiqSans-Regular", sans-serif;
@@ -247,7 +212,15 @@ export default {
   font-weight: 400;
   position: relative;
   flex: 1;
+  outline: none;
+
 }
+
+T-nguy-n :focus {
+  outline: none;
+  border: 0;
+}
+
 .arrow-drop-down {
   flex-shrink: 0;
   width: 26.63px;
@@ -255,6 +228,7 @@ export default {
   position: relative;
   overflow: visible;
 }
+
 .ghi-nh-n-c-n-h-m-i {
   color: #1d3557;
   text-align: center;
@@ -271,10 +245,12 @@ export default {
   align-items: center;
   justify-content: center;
 }
+
 .group-n-btn {
   position: static;
   inset: 0;
 }
+
 .rectangle-23 {
   background: #457b9d;
   border-radius: 10px;
@@ -290,6 +266,7 @@ export default {
   font-family: "Raleway-SemiBold", sans-serif;
   font-size: 28px;
 }
+
 .t-ng {
   color: #1d3557;
   text-align: left;
@@ -301,10 +278,12 @@ export default {
   left: 118px;
   top: 183px;
 }
+
 .t-i-a-ch {
   position: absolute;
   inset: 0;
 }
+
 .text-field {
   border-radius: 10px;
   display: flex;
@@ -318,6 +297,7 @@ export default {
   width: 26.08%;
   top: calc(50% - -34.5px);
 }
+
 .text-field2 {
   border-radius: 10px;
   display: flex;
@@ -329,21 +309,7 @@ export default {
   width: 292.07px;
   position: relative;
 }
-.frame4 {
-  background: var(--white, #ffffff);
-  border-radius: 4.43px;
-  border-style: solid;
-  border-color: var(--gray-200, #b3b3b3);
-  border-width: 1.11px;
-  display: flex;
-  flex-direction: column;
-  gap: 0px;
-  align-items: flex-start;
-  justify-content: flex-start;
-  align-self: stretch;
-  flex-shrink: 0;
-  position: relative;
-}
+
 .frame5 {
   padding: 13.28px 17.71px 13.28px 17.71px;
   display: flex;
@@ -355,6 +321,7 @@ export default {
   flex-shrink: 0;
   position: relative;
 }
+
 .nh-p-cccd-ch-h {
   color: var(--gray-100, #666666);
   text-align: left;
@@ -365,11 +332,13 @@ export default {
   position: relative;
   flex: 1;
 }
+
 .t-i-a-ch2 {
   width: 79px;
   height: 32px;
   position: static;
 }
+
 .c-n-h {
   color: #1d3557;
   text-align: left;
@@ -381,6 +350,7 @@ export default {
   left: 118px;
   top: 283px;
 }
+
 .ch-h {
   color: #1d3557;
   text-align: left;
@@ -392,6 +362,7 @@ export default {
   left: 118px;
   top: 368px;
 }
+
 .lo-i-h-nh {
   color: #1d3557;
   text-align: left;
@@ -403,6 +374,7 @@ export default {
   left: 569px;
   top: 183px;
 }
+
 .icon-x-letter {
   height: auto;
   position: absolute;
@@ -414,6 +386,7 @@ export default {
   height: 4.96%;
   overflow: visible;
 }
+
 .dropdown {
   display: flex;
   flex-direction: column;
@@ -428,6 +401,7 @@ export default {
   top: 28.84%;
   height: 7.91%;
 }
+
 .frame6 {
   background: var(--white, #ffffff);
   border-radius: 4.44px;
@@ -443,6 +417,7 @@ export default {
   flex-shrink: 0;
   position: relative;
 }
+
 .arrow-drop-down2 {
   flex-shrink: 0;
   width: 26.63px;
@@ -450,6 +425,7 @@ export default {
   position: relative;
   overflow: visible;
 }
+
 .dropdown2 {
   display: flex;
   flex-direction: column;
@@ -464,6 +440,7 @@ export default {
   top: 42.02%;
   height: 7.91%;
 }
+
 .arrow-drop-down3 {
   flex-shrink: 0;
   width: 26.63px;
@@ -471,6 +448,7 @@ export default {
   position: relative;
   overflow: visible;
 }
+
 .frame7 {
   background: var(--white, #ffffff);
   border-radius: 4.44px;
@@ -487,6 +465,7 @@ export default {
   left: 764px;
   top: 300px;
 }
+
 .arrow-drop-down4 {
   flex-shrink: 0;
   width: 26.63px;
@@ -494,6 +473,7 @@ export default {
   position: relative;
   overflow: visible;
 }
+
 .th-m-nh-n-kh-u {
   color: #1d3557;
   text-align: left;
