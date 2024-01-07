4d412921c9efd56fe1b548277b12f2df1fd28878
shirakamipc <duongnhatthanh2001@gmail.com>
2024-01-07 23:18:02 +0700

    form not complete

--- src/views/admin/form/AdminResponseFormView.vue
+++ src/views/admin/form/AdminResponseFormView.vue
@@ -1,162 +1,164 @@
 <template>
-  <div class="admin-ph-n-h-i-khi-u-n-i">
-    <svg
-      class="rectangle-30"
-      width="902"
-      height="840"
-      viewBox="0 0 902 840"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_d_1105_8252)">
-        <path
-          d="M0 9.99999C0 4.47714 4.47715 0 10 0H885C890.523 0 895 4.47715 895 10V823C895 828.523 890.523 833 885 833H10C4.47716 833 0 828.523 0 823V9.99999Z"
-          fill="#A8DADC"
-        />
-        <path
-          d="M10 1H885C889.971 1 894 5.02944 894 10V823C894 827.971 889.971 832 885 832H10C5.02944 832 1 827.971 1 823V9.99999C1 5.02943 5.02944 1 10 1Z"
-          stroke="black"
-          stroke-width="2"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_d_1105_8252"
-          x="0"
-          y="0"
+  <form>
+    <div class="admin-ph-n-h-i-khi-u-n-i">
+      <svg
+          class="rectangle-30"
           width="902"
           height="840"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
+          viewBox="0 0 902 840"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_d_1105_8252)">
+          <path
+              d="M0 9.99999C0 4.47714 4.47715 0 10 0H885C890.523 0 895 4.47715 895 10V823C895 828.523 890.523 833 885 833H10C4.47716 833 0 828.523 0 823V9.99999Z"
+              fill="#A8DADC"
           />
-          <feOffset dx="4" dy="4" />
-          <feGaussianBlur stdDeviation="1.5" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          <path
+              d="M10 1H885C889.971 1 894 5.02944 894 10V823C894 827.971 889.971 832 885 832H10C5.02944 832 1 827.971 1 823V9.99999C1 5.02943 5.02944 1 10 1Z"
+              stroke="black"
+              stroke-width="2"
           />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_1105_8252"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_1105_8252"
-            result="shape"
-          />
-        </filter>
-      </defs>
-    </svg>
+        </g>
+        <defs>
+          <filter
+              id="filter0_d_1105_8252"
+              x="0"
+              y="0"
+              width="902"
+              height="840"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+            />
+            <feOffset dx="4" dy="4"/>
+            <feGaussianBlur stdDeviation="1.5"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1105_8252"
+            />
+            <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1105_8252"
+                result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
 
-    <div class="x-c-nh-n-x-a">
-      <div class="rectangle-31"></div>
-      <div class="section-name">Phản hồi</div>
-    </div>
-    <div class="text-field">
-      <div class="text-field2">
-        <div class="frame">
-          <div class="frame2">
-            <div
-              class="ban-qu-n-l-ti-p-nh-n-ki-n-c-a-c-d-n-v-s-ph-i-h-p-v-i-ban-qu-n-tr-t-ch-c-trong-th-i-gian-s-m-nh-t"
-            >
-              Ban quản lý đã tiếp nhận ý kiến của cư dân và sẽ phối hợp với ban
-              quản trị để tổ chức trong
-              <br />
-              thời gian sớm nhất.
+      <div class="x-c-nh-n-x-a">
+        <div class="rectangle-31"></div>
+        <button class="section-name">Phản hồi</button>
+      </div>
+      <div class="text-field">
+        <div class="text-field2">
+          <div class="frame">
+            <div class="frame2">
+              <div
+                  class="ban-qu-n-l-ti-p-nh-n-ki-n-c-a-c-d-n-v-s-ph-i-h-p-v-i-ban-qu-n-tr-t-ch-c-trong-th-i-gian-s-m-nh-t"
+              >
+                Ban quản lý đã tiếp nhận ý kiến của cư dân và sẽ phối hợp với ban
+                quản trị để tổ chức trong
+                <br/>
+                thời gian sớm nhất.
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="frame3">
-      <div class="frame2">
-        <div
-          class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023"
-        >
-          <span>
-            <span
-              class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span"
-            >
-              Kiến nghị từ cư dân Lê Thanh Quang
-              <br />
-            </span>
-            <span
-              class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span2"
-            >
-              Về việc quản lý tiêm phòng chó dại
-              <br />
-              28/12/2023
-            </span>
-          </span>
+      <div class="frame3">
+        <div class="frame2">
+          <div
+              class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023"
+          >
+                <span>
+                  <span
+                      class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span"
+                  >
+                    Kiến nghị từ cư dân Lê Thanh Quang
+                    <br/>
+                  </span>
+                  <span
+                      class="ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span2"
+                  >
+                    Về việc quản lý tiêm phòng chó dại
+                    <br/>
+                    28/12/2023
+                  </span>
+                </span>
+          </div>
         </div>
       </div>
+      <div class="ph-n-h-i-ki-n-ngh-t-c-d-n-l-thanh-quang">
+        Phản hồi “Kiến nghị từ cư dân Lê Thanh Quang”
+      </div>
+      <svg
+          class="icon-x-letter"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_d_1105_8254)">
+          <path
+              d="M24 3.55275L22.6792 0.373047L14 21.2684L5.32085 0.373047L4 3.55275L12.6792 24.4482L4 45.3436L5.32085 48.5233L14 27.628L22.6792 48.5233L24 45.3436L15.3208 24.4482L24 3.55275Z"
+              fill="black"
+          />
+        </g>
+        <defs>
+          <filter
+              id="filter0_d_1105_8254"
+              x="0"
+              y="0.373047"
+              width="28"
+              height="56.1504"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+            />
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1105_8254"
+            />
+            <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1105_8254"
+                result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
     </div>
-    <div class="ph-n-h-i-ki-n-ngh-t-c-d-n-l-thanh-quang">
-      Phản hồi “Kiến nghị từ cư dân Lê Thanh Quang”
-    </div>
-    <svg
-      class="icon-x-letter"
-      width="28"
-      height="57"
-      viewBox="0 0 28 57"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_d_1105_8254)">
-        <path
-          d="M24 3.55275L22.6792 0.373047L14 21.2684L5.32085 0.373047L4 3.55275L12.6792 24.4482L4 45.3436L5.32085 48.5233L14 27.628L22.6792 48.5233L24 45.3436L15.3208 24.4482L24 3.55275Z"
-          fill="black"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_d_1105_8254"
-          x="0"
-          y="0.373047"
-          width="28"
-          height="56.1504"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_1105_8254"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_1105_8254"
-            result="shape"
-          />
-        </filter>
-      </defs>
-    </svg>
-  </div>
+  </form>
 </template>
 <script>
 /* Code generated with AutoHTML Plugin for Figma */
@@ -176,11 +178,13 @@ export default {
 .admin-ph-n-h-i-khi-u-n-i * {
   box-sizing: border-box;
 }
+
 .admin-ph-n-h-i-khi-u-n-i {
   width: 895px;
   height: 833px;
   position: relative;
 }
+
 .rectangle-30 {
   border-radius: 0px;
   width: 895px;
@@ -190,10 +194,12 @@ export default {
   top: 0px;
   overflow: visible;
 }
+
 .x-c-nh-n-x-a {
   position: absolute;
   inset: 0;
 }
+
 .rectangle-31 {
   background: var(--primary, #457b9d);
   border-radius: 15px;
@@ -205,6 +211,7 @@ export default {
   top: 88.24%;
   height: 6.7%;
 }
+
 .section-name {
   color: #f1faee;
   text-align: center;
@@ -222,7 +229,18 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  background: var(--primary, #457b9d);
+  border-radius: 15px;
+  right: 35.87%;
+  left: 34.01%;
+  width: 30.12%;
+  bottom: 5.06%;
+  top: 88.24%;
+  height: 6.7%;
+  border: none;
+  cursor: pointer;
 }
+
 .text-field {
   border-radius: 10px;
   display: flex;
@@ -236,6 +254,7 @@ export default {
   left: calc(50% - 397.5px);
   top: calc(50% - -21.5px);
 }
+
 .text-field2 {
   border-radius: 10px;
   display: flex;
@@ -247,6 +266,7 @@ export default {
   flex: 1;
   position: relative;
 }
+
 .frame {
   background: var(--white, #ffffff);
   border-radius: 4.43px;
@@ -262,6 +282,7 @@ export default {
   flex: 1;
   position: relative;
 }
+
 .frame2 {
   padding: 13.28px 17.71px 13.28px 17.71px;
   display: flex;
@@ -273,6 +294,7 @@ export default {
   flex: 1;
   position: relative;
 }
+
 .ban-qu-n-l-ti-p-nh-n-ki-n-c-a-c-d-n-v-s-ph-i-h-p-v-i-ban-qu-n-tr-t-ch-c-trong-th-i-gian-s-m-nh-t {
   color: var(--gray-100, #666666);
   text-align: left;
@@ -282,6 +304,7 @@ export default {
   font-weight: 400;
   position: relative;
 }
+
 .frame3 {
   background: var(--cream, #f1faee);
   border-radius: 4.43px;
@@ -299,6 +322,7 @@ export default {
   left: 47px;
   top: 124px;
 }
+
 .ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023 {
   color: var(--secondary, #1d3557);
   text-align: left;
@@ -308,6 +332,7 @@ export default {
   font-weight: 400;
   position: relative;
 }
+
 .ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span {
   color: var(--secondary, #1d3557);
   font-family: "Raleway-SemiBold", sans-serif;
@@ -315,6 +340,7 @@ export default {
   line-height: 64px;
   font-weight: 600;
 }
+
 .ki-n-ngh-t-c-d-n-l-thanh-quang-v-vi-c-qu-n-l-ti-m-ph-ng-ch-d-i-28-12-2023-span2 {
   color: var(--secondary, #1d3557);
   font-family: "Raleway-Regular", sans-serif;
@@ -322,6 +348,7 @@ export default {
   line-height: 64px;
   font-weight: 400;
 }
+
 .ph-n-h-i-ki-n-ngh-t-c-d-n-l-thanh-quang {
   color: #1d3557;
   text-align: center;
@@ -335,6 +362,7 @@ export default {
   width: 800px;
   height: 60px;
 }
+
 .icon-x-letter {
   height: auto;
   position: absolute;
