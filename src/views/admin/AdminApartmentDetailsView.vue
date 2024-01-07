16c73c4e6496af1f3965de1b8a9d42752e62bb01
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-07 17:26:51 +0700

    feat: create list that contains residents

--- src/views/admin/AdminApartmentDetailsView.vue
+++ src/views/admin/AdminApartmentDetailsView.vue
@@ -1,31 +1,30 @@
 <script setup>
 // import Card from '@/components/Card.vue';
 import ApartmentProfile from '@/components/Profile/ApartmentProfile.vue';
-import ApartmentResidentItem from '@/components/Profile/ApartmentResidentItem.vue';
-import ExtraProfileCardItem from '@/components/Profile/ExtraProfileCardItem.vue';
+// import ApartmentResidentItem from '@/components/Profile/ApartmentResidentItem.vue';
+// import ExtraProfileCardItem from '@/components/Profile/ExtraProfileCardItem.vue';
+import ResidentItemList from '@/components/Profile/ResidentItemList.vue';
 // import AccountProfile from '@/components/Profile/AccountProfile.vue';
-
-
 </script>
 
 <template>
-  <main>
-    <div class="row">
-      <div class="col-8">
-        <ApartmentProfile></ApartmentProfile>
-      </div>
-      <div class="col-4">
-          <h4 class="card-title">Cư dân căn hộ</h4>
-          <ApartmentResidentItem />
-          <ExtraProfileCardItem />
-      </div>
-    </div>
-  </main>
+    <main>
+        <div class="row">
+            <div class="col-8">
+                <ApartmentProfile></ApartmentProfile>
+            </div>
+            <div class="col-4">
+                <h4 class="card-title">Cư dân căn hộ</h4>
+                <ResidentItemList />
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped>
-  .card-title {
+.card-title {
     text-align: center;
     font: 30px/150% Raleway, sans-serif;
-  }
+    color: var(--main-secondary);
+}
 </style>
