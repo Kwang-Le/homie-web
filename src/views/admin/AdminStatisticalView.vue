297506ea1652562c902b0fe24eb001e8d0b4ea8a
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-07 14:47:34 +0700

    feat: create apartment details view

--- src/views/admin/AdminStatisticalView.vue
+++ src/views/admin/AdminStatisticalView.vue
@@ -1,29 +1,12 @@
 <script setup>
-// import Card from '@/components/Card.vue';
-import ApartmentProfile from '@/components/Profile/ApartmentProfile.vue';
-import ApartmentResidentItem from '@/components/Profile/ApartmentResidentItem.vue';
-import ExtraProfileCardItem from '@/components/Profile/ExtraProfileCardItem.vue';
-// import AccountProfile from '@/components/Profile/AccountProfile.vue';
+
 </script>
 
 <template>
   <main>
-    <div class="row">
-      <div class="col-8">
-        <ApartmentProfile></ApartmentProfile>
-      </div>
-      <div class="col-4">
-          <h4 class="card-title">Cư dân căn hộ</h4>
-          <ApartmentResidentItem />
-          <ExtraProfileCardItem />
-      </div>
-    </div>
+
+
   </main>
 </template>
 
-<style scoped>
-  .card-title {
-    text-align: center;
-    font: 30px/150% Raleway, sans-serif;
-  }
-</style>
+<style></style>
