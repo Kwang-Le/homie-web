1cab6061798f39a01bd6423a40999458e313bd0b
shirakamipc <duongnhatthanh2001@gmail.com>
2024-01-08 00:11:23 +0700

    form not complete

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import AdminStatisticalView from '@/views/admin/AdminStatisticalView.vue'
 import AdminLoginFormView from '@/views/admin/form/AdminLoginFormView.vue'
 import AdminResetPasswordFormView from '@/views/admin/form/AdminResetPasswordFormView.vue'
 import AdminCodeConfirmFormView from '@/views/admin/form/AdminCodeConfirmFormView.vue'
-import AdminNewPassWordFormView from '@/views/admin/form/AdminNewPassWordFormView.vue'
+import AdminNewPasswordFormView from '@/views/admin/form/AdminNewPassWordFormView.vue'
 import AdminRegisterFormView from '@/views/admin/form/AdminRegisterFormView.vue'
 import AdminNewApartmentFormView from '@/views/admin/form/AdminNewApartmentFormView.vue'
 import AdminCreateAccountFormView from "@/views/admin/form/AdminCreateAccountFormView.vue";
@@ -87,7 +87,7 @@ const router = createRouter({
     {
         path: '/admin-new-password-form',
         name: 'admin-new-password-form',
-        component: AdminNewPassWordFormView
+        component: AdminNewPasswordFormView
     },
     {
         path: '/admin-register-form',
