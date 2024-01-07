ef4ec71584a4b6cf95dddd3a98f4b4bd50712f03
rellasie <86995253+rellasie@users.noreply.github.com>
2024-01-07 19:59:08 +0700

    feat: create Fee Details view

---
+++ src/views/admin/AdminFeeDetailsView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <div class="row">
+            <div class="col-8">
+                <FeeProfile></FeeProfile> <!-- TODO -->
+            </div>
+            <div class="col-4">
+                <h4 class="card-title">Các khoản khác</h4> 
+                <ExtraFeesList /> <!-- TODO -->
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.card-title {
+    text-align: center;
+    font: 30px/150% Raleway, sans-serif;
+    color: var(--main-secondary);
+}
+</style>
