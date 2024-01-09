772ad26e0627dfee5d2008883d00fb6c8026fa0c
shirakamipc <duongnhatthanh2001@gmail.com>
2024-01-09 14:53:38 +0700

    form complete but still have some mini bugs

---
+++ src/views/admin/form/AdminRecordFormView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AdminRecordFormView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/admin/form/AdminRecordListView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AdminRecordListView"
+}
+</script>
+
+<style scoped>
+
+</style>
