77bfe4f926512966d8dc92e04cdb419bb286e1af
Kwang_Le <quangle280302@gmail.com>
2024-01-09 21:44:23 +0700

    add default route to login when visiting the website

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: 'login',
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginViewVue
